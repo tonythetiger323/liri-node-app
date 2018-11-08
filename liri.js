6b4ad45e3ff2b47d582d4a99a3b59e412b645efb
Tony Greeley <tony.greeley@me.com>
2018-11-08 06:58:07 -0600

    minor adjustments

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//required
 const dotenv = require('dotenv').config();
 const fs = require('fs');
 const request = require('request');
@@ -5,6 +6,7 @@ const moment = require('moment');
 const nodeSpotifyApi = require('node-spotify-api');
 const keys = require('./keys.js');
 
+//keys
 const bandApi = keys.band.id;
 const spotify = new nodeSpotifyApi({
     id: keys.spotify.id,
@@ -12,10 +14,12 @@ const spotify = new nodeSpotifyApi({
 });
 const omdbApi = keys.omdb.key;
 
+//command line input
 let liriArgs = process.argv;
 let liriCommand = process.argv[2];
 let liriParam = liriArgs.slice(3).join(' ');
 
+//function to append log.txt file
 function logtxt(output) {
     fs.appendFile(`log.txt`, `${liriCommand} ${liriParam}\n${output}\n\n`, `utf8`, function (err) {
         if (err) {
@@ -26,14 +30,13 @@ function logtxt(output) {
     });
 }
 
+//function for concert-this command
 function concertThis(artist) {
-    console.log(artist);
     const queryUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${bandApi}`;
 
     request(queryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             const event = JSON.parse(body)[0];
-            console.log(event);
             const output = `Name of Venue: ${event.venue.name}\nVenue Location: ${event.venue.city}\nDate of Event: ${moment(event.datetime).format("MM/DD/YYYY")}`;
             console.log(output);
             logtxt(output);
@@ -42,9 +45,9 @@ function concertThis(artist) {
     });
 }
 
+//function for spotify-this-song command
 function spotifyThisSong(song) {
-    console.log(song);
-    if (song === undefined) {
+    if (!song) {
         song === `The Sign by Ace of Base`;
     }
     spotify.search({ type: `track`, query: song, limit: 10 }, function (err, data) {
@@ -54,9 +57,9 @@ function spotifyThisSong(song) {
     });
 }
 
+//function for movie-this command
 function movieThis(movie) {
-    console.log(movie);
-    if (movie === undefined) {
+    if (!movie) {
         movie === `Mr. Nobody`;
     }
 
@@ -74,6 +77,7 @@ function movieThis(movie) {
 
 }
 
+//function for do-what-it-says command
 function doWhatItSays() {
     fs.appendFile(`log.txt`, `${liriCommand}\n`, `utf8`, function (err) {
         if (err) {
@@ -83,14 +87,13 @@ function doWhatItSays() {
         }
     });
     const readFileArray = fs.readFileSync(`random.txt`).toString().split(',');
-    console.log(readFileArray);
     const command = readFileArray[0];
-    let paramater = readFileArray[1];
-    paramater = paramater.replace(/["]/g, "");
+    const paramater = readFileArray[1].replace(/["]/g, "");
 
     heyLiri(command, paramater);
 
 }
+//function with switch syntax so each function runs based on command input
 function heyLiri(command, param) {
     switch (command) {
         case `concert-this`:
@@ -108,4 +111,5 @@ function heyLiri(command, param) {
     }
 }
 
+//initialize app
 heyLiri(liriCommand, liriParam);
